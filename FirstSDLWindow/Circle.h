ac9b9a0f2c67fc97f4c955a6cbc3a94d12b163a0
Benjamin Belandres <bbelandr@vols.utk.edu>
2025-03-25 15:48:00 -0400

    quick edit for constructor inheritance

--- FirstSDLWindow/Circle.h
+++ FirstSDLWindow/Circle.h
@@ -3,6 +3,7 @@
 class Circle : public Object {
 public:
 	float radius;
+	using Object::Object;
 
-	Circle(float x, float y, float radius) : Object(x, y);
+	Circle(float x, float y, float radius);
 };
