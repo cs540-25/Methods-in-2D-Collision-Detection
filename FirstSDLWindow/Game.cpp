ac4453d0fb20c1564f1c7b7ab90e1b0c0ce67525
Benjamin Belandres <bbelandr@vols.utk.edu>
2025-05-05 22:51:25 -0400

    Wording

--- FirstSDLWindow/Game.cpp
+++ FirstSDLWindow/Game.cpp
@@ -66,7 +66,7 @@ Game::~Game() {
 		printf("Maximum Framerate:     %20.10f\n", maxFPS);
 		printf("Minimum Framerate:     %20.10f\n", minFPS);
 		printf("Framerate Variability: %20.10f\n", maxFPS - minFPS);
-		printf("Framerate per Object:  %20.10f\n", (float)totalFrames / objects.size());
+		printf("Frames per Object:     %20.10f\n", (float)totalFrames / objects.size());
 		//std::string response;
 		//std::cout << "Enter any character to close: ";
 		//std::cin >> response;
