05c546dbe2b8706a393b825cde04568ee31016dc
Benjamin Belandres <bbelandr@vols.utk.edu>
2025-05-07 20:36:18 -0400

    commenting

--- FirstSDLWindow/UniformGrid.h
+++ FirstSDLWindow/UniformGrid.h
@@ -9,8 +9,8 @@ public:
 	UniformGrid(int cellWidth, int cellHeight, int windowWidth, int windowHeight);
 
 	vector getCell(const vector& pos);	// Returns a vector that has the x position and y position of the cell you're looking for
-	void setCells(Object* a);
-	void clearCells();
+	void setCells(Object* a);			// Sets all of the cells that the object a would be in
+	void clearCells();					// Clears all of the cells in the uniformGrid.
 
 };
 
