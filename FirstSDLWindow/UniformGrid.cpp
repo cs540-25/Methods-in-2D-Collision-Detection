f900b16db98d2e82df2d101f08412c03a8305de3
Benjamin Belandres <bbelandr@vols.utk.edu>
2025-05-07 21:58:59 -0400

    fixing indexing things out of scope

--- FirstSDLWindow/UniformGrid.cpp
+++ FirstSDLWindow/UniformGrid.cpp
@@ -14,7 +14,7 @@ UniformGrid::UniformGrid(int cellWidth, int cellHeight, int windowWidth, int win
 	
 }
 
-vector UniformGrid::getCell(const vector& pos)
+vector UniformGrid::getCell(const vector& pos)	// This does not error check if the return cell is out of bounds
 {
 	return vector((int)pos.x/cellWidth, (int)pos.y/cellHeight);
 }
@@ -31,7 +31,9 @@ std::set<Object*> UniformGrid::setCellsAndScoutCollision(Object* a)	// Returns a
 	vector max = getCell(a->AABB->max());
 	std::set<Object*> ret;
 	for (int i = (int)min.x; i <= (int)max.x; i++) {
+		if (i > uniformGrid.size() || i < 0) continue;	// Skipping if i falls out of bounds
 		for (int j = (int)min.y; j <= (int)max.y; j++) {
+			if (j > uniformGrid[i].size() || j < 0) continue;	// Skipping if j falls out of bounds
 			for (auto k = 0; k < uniformGrid[i][j].size(); k++) {	// Adding the objects that are already in the vector to the set of possible collisions
 				ret.insert(uniformGrid[i][j][k]);
 			}
